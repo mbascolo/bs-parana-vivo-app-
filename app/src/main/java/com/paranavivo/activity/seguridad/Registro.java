e80c472259ed808739ee567619f7d8d831a3c24e
Claudio Trosch <claudiotrosch@gmail.com>
2016-09-28 23:26:02 -0300

    Implementando registro y login

---
+++ app/src/main/java/com/paranavivo/activity/seguridad/Registro.java
@@ -0,0 +1,22 @@
+package com.paranavivo.activity.seguridad;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.paranavivo.R;
+import com.paranavivo.utils.ActivityBase;
+
+public class Registro extends ActivityBase implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+
+    @Override
+    public void onClick(View v) {
+        
+    }
+}
